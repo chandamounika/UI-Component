3409c5536d24b150fbbf968e40428edad1895b73
Mitesh Mankame <Mankame.Mitesh@azdeq.gov>
2020-10-09 16:49:48 -0700

    Update environment.ts

--- external/src/static/environments/environment.ts
+++ external/src/static/environments/environment.ts
@@ -31,6 +31,7 @@ export const environment = {
 };
 
 /*
+* - Sample Comment
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
